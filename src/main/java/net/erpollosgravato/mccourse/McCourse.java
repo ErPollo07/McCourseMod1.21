2e0f0e38d5da65b2562464446938696bea932788
ErPollo07 <nicola.tosatti@iisviolamarchesini.edu.it>
2025-05-10 18:48:01 +0200

    Init commit

---
+++ src/main/java/net/erpollosgravato/mccourse/McCourse.java
@@ -0,0 +1,15 @@
+package net.erpollosgravato.mccourse;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class McCourse implements ModInitializer {
+	public static final String MOD_ID = "mccourse";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/erpollosgravato/mccourse/McCourseClient.java
@@ -0,0 +1,10 @@
+package net.erpollosgravato.mccourse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class McCourseClient implements ClientModInitializer {
+  @Override
+  public void onInitializeClient() {
+
+  }
+}
